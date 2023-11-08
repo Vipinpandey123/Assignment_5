f74d64feb0c0e24b66fa904b8732dcd0b16ce827
Vipinpandey123 <142861510+Vipinpandey123@users.noreply.github.com>
2023-11-08 09:34:57 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_5
+{
+    partial class Except
+    {
+        public void Format()
+        {
+            string s = "Hello";
+            try
+            {
+                int a  = Convert.ToInt32(s);
+                Console.WriteLine(a);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
